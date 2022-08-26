0e627b115341bd6ca4454ab144b4ae82200294ff
jaeger2801 <juan.lopez48@u.icesi.edu.co>
2022-08-26 08:57:39 -0500

    Crea modificcaciones disposicion página

--- to_do_list.js
+++ to_do_list.js
@@ -1,1 +1,12 @@
-console.log("hola");
+const allTaskList = document.getElementsByClassName("all_task");
+
+/// Create the arrays
+const allTaskListNav = [
+    {text:"Sacar la basura a las 9 am"},
+    {text:"Comprar 500 de cilantro"},
+    {text:"Lavar la ropa"},
+    {text:"Limpiar arenero de los gatos"},
+    {text:"Visitar a los panas"},
+]
+
+console.log(allTaskListNav);
